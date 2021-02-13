4e2878f4d98bf75631996b493dedda73321c2660
Denny da Vjncj <daVjncjCode@gmail.com>
2021-02-13 17:03:30 -0500

    updated imports

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,12 +1,23 @@
-import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+const projects=[{
+    id:1,
+    title:'final project'
+}];//mimicks a database that i'm pulling projects from
 
 const HomePage=()=>{
     return(
-        <div>
-            <p>
-                HomePage
-            </p>
-        </div>
+        <Container>            
+            <Row md={4}>
+                {/* <Col>first project</Col>
+                <Col md={4}>second project</Col>
+                <Col>3 of 3</Col> */}
+                {projects.map(project=>(
+                    <Col key={project.id}>{project.title}</Col>
+                ))}
+            </Row>
+        </Container>        
     )
 }
 export default HomePage;
