9d3f621ef55294b3e6b1c593c6c638e78b13b6e9
Denny da Vjncj <daVjncjCode@gmail.com>
2021-03-23 17:50:05 -0400

    updates to rendering stack

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
-import Header from './particles/Header';
 import HomePage from './HomePage';
 import Contact from "./Contact";
 import { HashRouter as Router, Switch, Route } from 'react-router-dom';
+import ProminentAppBar from './particles/Header';
 
 function App() {
   return (
     <Router basename="/">
-      <Header/>
+      <ProminentAppBar/>
       <Switch>
         <Route exact path='/' component={HomePage}/>
         <Route exact path='/contact' component={Contact}/>
