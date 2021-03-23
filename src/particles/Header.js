d78fdc797648aace9c0949ceb9ee059dba8e5dc9
Denny da Vjncj <daVjncjCode@gmail.com>
2021-03-23 17:51:09 -0400

    tentative updates to header component/adding in theme for prominent app bar

--- src/particles/Header.js
+++ src/particles/Header.js
@@ -1,28 +1,46 @@
 import React from 'react';
 import {Link} from "react-router-dom";
-import {lightBlue,red} from '@material-ui/core/colors';
-import {Container,Grid,Paper,Button,AppBar,Toolbar,IconButton,Typography} from '@material-ui/core';
-import {MenuIcon,SearchIcon,MoreIcon} from '@material-ui/icons';
+import {MenuIcon} from '@material-ui/icons/Menu';
+import {Container,
+        Grid,
+        Button,
+        AppBar,
+        Toolbar,
+        IconButton,
+        Typography,
+        makeStyles} from '@material-ui/core';
 
+const useStyles=makeStyles(theme=>({
+  root:{
+    flexGrow:1,
+  },
+  menuButton:{
+    marginRight:theme.spacing(2),
+  },
+  toolbar:{
+    minHeight:128,
+    alignItems:'flex-start',
+    paddingTop:theme.spacing(1),
+    paddingBottom:theme.spacing(2),
+  },
+  title:{
+    flexGrow:1,
+    alignSelf:'flex-end',
+  },
+}));
+export default function ProminentAppBar(){
+  const classes=useStyles();
 
-
-
-
-const Header=()=>{
   return(
-    <header>
-      <Container maxWidth='sm'>
-        <Grid container>
-          <Grid item xs={12}>
-            {/* <ThemeProvider useStyles={theme}> */}
-              <Paper elevation={3}>
-                testing & proving
-              </Paper>
-            {/* </ThemeProvider> */}
-          </Grid>
-        </Grid>
-      </Container>
-    </header>
-  )
+    <div className={classes.root}>
+      <AppBar position='static'>
+        <Toolbar className={classes.toolbar}>
+          <IconButton edge='start'className={classes.menuButton}color='inherit'aria-label='open drawer'>
+            <MenuIcon/>
+          </IconButton>
+          
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
 }
-export default Header;
