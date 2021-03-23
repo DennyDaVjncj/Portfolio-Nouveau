277c5ba3e2d4879dcb428f98d989fb652859aa74
Denny da Vjncj <daVjncjCode@gmail.com>
2021-03-23 10:49:27 -0400

    new components for reconstruction

--- src/particles/Header.js
+++ src/particles/Header.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import {Link} from "react-router-dom";
+import {lightBlue,red} from '@material-ui/core/colors';
+import {Container,Grid,Paper,Button,AppBar,Toolbar,IconButton,Typography} from '@material-ui/core';
+import {MenuIcon,SearchIcon,MoreIcon} from '@material-ui/icons';
+
+
+
+
 
 const Header=()=>{
   return(
-    <div>
-
-    </div>
+    <header>
+      <Container maxWidth='sm'>
+        <Grid container>
+          <Grid item xs={12}>
+            {/* <ThemeProvider useStyles={theme}> */}
+              <Paper elevation={3}>
+                testing & proving
+              </Paper>
+            {/* </ThemeProvider> */}
+          </Grid>
+        </Grid>
+      </Container>
+    </header>
   )
 }
 export default Header;
