ff1fd360fe187b8f9fc8ba9f0e9af014a9517371
Denny da Vjncj <daVjncjCode@gmail.com>
2021-04-01 15:58:55 -0400

    updated component names

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import RecipeReviewCard from './HomePage';
+import MediaCard from './HomePage';
 import Contact from "./Contact";
 import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 import ProminentAppBar from './particles/Header';
@@ -9,7 +9,7 @@ function App() {
     <Router basename="/">
       <ProminentAppBar/>
       <Switch>
-        <Route exact path='/' component={RecipeReviewCard}/>
+        <Route exact path='/' component={MediaCard}/>
         <Route exact path='/contact' component={Contact}/>
       </Switch>
     </Router>
