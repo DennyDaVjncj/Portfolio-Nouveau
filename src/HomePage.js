8e6d4e367585d675a008150887de4ef56e3a9c11
Denny da Vjncj <daVjncjCode@gmail.com>
2021-04-01 15:57:09 -0400

    updated ui  component

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,97 +1,54 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import clsx from 'clsx';
 import Card from '@material-ui/core/Card';
-import CardHeader from '@material-ui/core/CardHeader';
-import CardMedia from '@material-ui/core/CardMedia';
-import CardContent from '@material-ui/core/CardContent';
+import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
-import Collapse from '@material-ui/core/Collapse';
-import Avatar from '@material-ui/core/Avatar';
-import IconButton from '@material-ui/core/IconButton';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
-import { red } from '@material-ui/core/colors';
-import FavoriteIcon from '@material-ui/icons/Favorite';
-import ShareIcon from '@material-ui/icons/Share';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import MoreVertIcon from '@material-ui/icons/MoreVert';//what is this?
-//mimicks a database that i'm pulling projects from
 
-const useStyles=makeStyles(theme=>({
+const useStyles=makeStyles({
     root:{
-        maxWidth:345,
+        maxWidth:666,        
     },
     media:{
-        height:0,
-        paddingTop:'56.25%',//16:9
-    },
-    expand:{
-        transform:'rotate(0deg)',
-        marginLeft:'auto',
-        transition:theme.transitions.create('transform',{
-            duration:theme.transitions.duration.shortest,
-        }),
-    },
-    expandOpen:{
-        transform:'rotate(180deg)'
-    },
-    avatar:{
-        backgroundColor:red[500],
-    },
-}));
-export default function RecipeReviewCard(){
-    const classes=useStyles()
-    const [expanded,setExpanded]=React.useState(false)
-    const handleExpandClick=()=>{
-        setExpanded(!expanded)
+        height:499
     }
+});
+export default function MediaCard(){
+    const classes=useStyles();
+
 return (
-    <div>               
-        <Card className={classes.root}>
-            <CardHeader avatar={
-                <Avatar aria-label='recipe'className={classes.avatar}>
-                    V
-                </Avatar>
-            }
-            action={
-                <IconButton aria-label='settings'>
-                    <MoreVertIcon/>
-                </IconButton>
-            }
-            title='Shrimp and Chorizo Paella'
-            subheader='September 14, 2016'/>
-            <CardMedia className={classes.media}image='/static/images/cards/paella.jpg'title='Paella dish'/>
-            <CardContent>
-                <Typography variant='body2'color='textSecondary'component='p'>
-                    Software engineering is the perfect mix of technical prowless & unlimeted creativity
-                </Typography>
-            </CardContent>
-            <CardActions disableSpacing>
-                <IconButton aria-label='add to favorites'>
-                    <FavoriteIcon/>
-                </IconButton>
-                <IconButton aria-label='share'>
-                    <ShareIcon/>
-                </IconButton>
-                <IconButton className={clsx(classes.expand,{
-                    [classes.expandOpen]:expanded,
-                })}
-                onClick={handleExpandClick}
-                aria-expanded={expanded}
-                aria-label='show more'>
-                    <ExpandMoreIcon/>
-                </IconButton>
-            </CardActions>
-            <Collapse in={expanded}timeout='auto'unmountOnExit>
-                <CardContent>
-                    <Typography paragraph>Methodology:</Typography>
-                    <Typography paragraph>
-                        aim, shoot... fire!
-                    </Typography>
-                </CardContent>
-            </Collapse>
-        </Card>                            
-    </div>)}
+    <Card className={classes.root}>
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image="/static/images/cards/contemplative-reptile.jpg"
+          title="Contemplative Reptile"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            Lizard
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+            across all continents except Antarctica
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          Share
+        </Button>
+        <Button size="small" color="primary">
+          Learn More
+        </Button>
+      </CardActions>
+    </Card>
+  );
+}
+
 {/* <Card raised={true}>
     <img src="https://pbs.twimg.com/media/EBxrWWHXkAIU9-N.jpg"/>
 </Card>
