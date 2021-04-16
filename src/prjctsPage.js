a1a641cc24ecd387d2e2242b49c1562cab2b2722
Denny da Vjncj <daVjncjCode@gmail.com>
2021-04-15 21:56:06 -0400

    tentative prjct thumbnaill render

--- src/prjctsPage.js
+++ src/prjctsPage.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Grid,Paper,Container} from '@material-ui/core';
+import thumbNail from './imagery/workoutTrackerThumbNail.jpg'
 
 let portfolio=()=>{
   return(
@@ -8,7 +9,7 @@ let portfolio=()=>{
       <Grid container spacing={9}>
         <Grid item xs={6}>
           <Paper>
-            'hey, my name is denny da vjncj'
+            <img src={thumbNail}/>
           </Paper>
         </Grid>
         <Grid item xs={6}>
