91ee88cdb271502f81950ad521bb79be95e7e7b5
Denny da Vjncj <daVjncjCode@gmail.com>
2021-04-20 19:37:31 -0400

    version control

--- src/HomePage.js
+++ src/HomePage.js
@@ -54,14 +54,14 @@ return (
             </Link>
           </Button>
           <Button size="small" color="primary">
+            <a href='https://docs.google.com/document/d/1qz9Qo_xlnGK4BvF6TUqJBfRB28xr-1p1lK_QK-7K16U/edit?pli=1'target='_blank'>
+              my Resume
+            </a>{/*pdf */}
+          <Button size="small" color="primary">
             <a href='https://github.com/DennyDaVjncj'target='_blank'>
               Github
             </a>
           </Button>
-          <Button size="small" color="primary">
-            <a href='https://docs.google.com/document/d/1qz9Qo_xlnGK4BvF6TUqJBfRB28xr-1p1lK_QK-7K16U/edit?pli=1'target='_blank'>
-              the resume
-            </a>
           </Button>
           <Button size="small" color="primary">
             <a href='https://www.linkedin.com/in/ambiguousambition/'target='_blank'>
--- src/prjctsPage.js
+++ src/prjctsPage.js
@@ -1,35 +1,57 @@
 import React from 'react';
-import {Grid,Paper,Container} from '@material-ui/core';
-import workoutTracker from './imagery/reSizedGoku.jpg'
+import {Grid,Paper,Container,CardContent,Typography,Card,CardActionArea,CardActions,CardMedia,Button, makeStyles} from '@material-ui/core';
+import workoutTracker from './imagery/reSizedGoku.jpg';
 import iSpend from './imagery/iSpend.png';
-import {ReactComponent as Spherical} from './imagery/globeIconSVG.svg';
+import Earth from './imagery/earth.png';
+
+let useStyles=makeStyles({
+  root:{
+    maxWidth:400
+  }
+})
 
 let portfolio=()=>{
   return(
-  <div>
+  <div className='div'>
     <Container maxWidth='sm'>{/**showcase credentials */}
-      <Grid container spacing={10}justify='space-around'>
-        <Grid item xs={4}>
-          <Paper>
-            <a href='https://stormy-tundra-80286.herokuapp.com/?id=600ccffa14955b0015415ea5'>
-              <img src={workoutTracker}/>
-            </a>
-          </Paper>
+      <Grid container spacing={10}>
+        <Grid item xs={8}>
+          <Card>
+            <CardActionArea>
+              <CardMedia component='img'alt='Goku getting it in'height='250'image={workoutTracker}title='Circuit training tracker'/>       
+              <CardContent>
+                <Typography gutterBottom variant='h4'component='h2'>
+                  Circuit Training Tracker
+                </Typography>{/**add in tools used to build app */}
+                <Typography variant='body2'color='textSecondary'component='p'>
+                  This app is designed to help folks serious about their fitness track their progress/regression
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+            <CardActions>
+              <Button size='small'color='primary'>
+                Deployed app
+              </Button>
+              <Button size='small'color='primary'>
+                Explore the repository
+              </Button>
+            </CardActions>
+          </Card>          
+          
         </Grid>
-        <Grid item xs={4}>
+        {/* <Grid item xs={4}>
           <Paper>
             <a href='https://agile-headland-12732.herokuapp.com/'target='_blank'>
               <img src={iSpend}/>
-            </a>{/**consider using a filled out card component to render prjct icons */}
+            </a>
           </Paper>
-          <Grid item xs={4}>
-            <Paper>
-              <a href='https://theNewCool.org'target='_blank'>
-                <Spherical/>
-              </a>
-            </Paper>
+        </Grid> */}
+          <Grid item xs={4}>            
+            <a href='https://theNewCool.org'target='_blank'>
+              <img src={Earth}/>
+            </a>            
           </Grid>          
-        </Grid>
+        
       </Grid>
     </Container>
   </div>
