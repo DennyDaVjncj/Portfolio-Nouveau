d84f35d1093b87381e9c33ab59c605621ef83b2b
Denny da Vjncj <daVjncjCode@gmail.com>
2021-04-21 18:00:42 -0400

    updates to prjctsPage display

--- src/prjctsPage.js
+++ src/prjctsPage.js
@@ -1,49 +1,66 @@
 import React from 'react';
-import {Grid,Paper,Container,CardContent,Typography,Card,CardActionArea,CardActions,CardMedia,Button, makeStyles} from '@material-ui/core';
+import {Grid,Paper,Container,CardContent,Typography,Card,CardActionArea,CardActions,CardMedia,Button,makeStyles} from '@material-ui/core';
 import workoutTracker from './imagery/reSizedGoku.jpg';
 import iSpend from './imagery/iSpend.png';
-import Earth from './imagery/earth.png';
+import Peacock from './imagery/peacock.svg';
 
 let useStyles=makeStyles({
   root:{
+    height:400,
     maxWidth:400
   }
-})
+});
 
 let portfolio=()=>{
   return(
   <div className='div'>
     <Container maxWidth='sm'>{/**showcase credentials */}
       <Grid container spacing={10}>
-        <Grid item xs={8}>
-          <Card>
-            <CardActionArea>
-              <CardMedia component='img'alt='Goku getting it in'height='250'image={workoutTracker}title='Circuit training tracker'/>       
-              <CardContent>
-                <Typography gutterBottom variant='h4'component='h2'>
-                  Circuit Training Tracker
-                </Typography>{/**add in tools used to build app */}
-                <Typography variant='body2'color='textSecondary'component='p'>
-                  This app is designed to help folks serious about their fitness track their progress/regression
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-            <CardActions>
-              <Button size='small'color='primary'>
-                <a href='https://stormy-tundra-80286.herokuapp.com/?id=600ccffa14955b0015415ea5'target='_blank'>
-                  Deployed app
-                </a>
-              </Button>
-              <Button size='small'color='primary'>
-                <a href='https://github.com/DennyDaVjncj/circuitTrainingTracker'target='_blank'>
-                  le repository
-                </a>
-              </Button>
-            </CardActions>
-          </Card>         
+        <Grid item xs={7}display='row'justify='space-evenly'alignContent='flex-end'>
+          <Paper elevation={24}>
+            <Card>
+              <CardActionArea>
+                <Paper elevation={9}>{/** */}
+                  <CardMedia component='img'alt='Goku getting it in'height='250'image={workoutTracker}title='Circuit training tracker'/>
+                </Paper>
+                <CardContent>
+                  <Typography gutterBottom variant='h4'component='h2'>
+                    Circuit Training Tracker
+                  </Typography>{/**add in tools used to build app */}
+                  <Typography variant='body2'color='textSecondary'component='p'>
+                    This app is designed to help folks serious about their fitness track their progress/regression
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+              <CardActions>
+                <Button size='small'color='primary'>
+                  <a href='https://stormy-tundra-80286.herokuapp.com/?id=600ccffa14955b0015415ea5'target='_blank'>
+                    Deployed app
+                  </a>
+                </Button>
+                <Button size='small'color='primary'>
+                  <a href='https://github.com/DennyDaVjncj/circuitTrainingTracker'target='_blank'>
+                    le repository
+                  </a>
+                </Button>
+              </CardActions>
+            </Card>
+          </Paper>
         </Grid>
 
-        <Grid
+        <Grid item xs={5}>
+          <div>
+            <Card>
+              <CardActionArea>
+                <CardMedia component='img'image={Peacock}alt='a peacock'>
+                  <a href='https://theNewCool.org'target='_blank'>
+                    <img className='global' src={Peacock}/>
+                  </a>
+                </CardMedia>
+              </CardActionArea>
+            </Card>
+          </div>          
+        </Grid>     
         
         {/* <Grid item xs={4}>
           <Paper>
@@ -52,14 +69,6 @@ let portfolio=()=>{
             </a>
           </Paper>
         </Grid> */}
-          <Grid item xs={4}>
-            <div>
-              <a href='https://theNewCool.org'target='_blank'>
-                <img src={Earth}/>
-              </a>{/*give div a universe type background to give icon 'floating in interdimension' feel */}
-            </div>          
-          </Grid>          
-        
       </Grid>
     </Container>
   </div>
