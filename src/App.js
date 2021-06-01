a3978d7f0518df68026e8d19dac7aa694dac6485
Denny da Vjncj <daVjncjCode@gmail.com>
2021-06-01 16:42:05 -0400

    updated rendering stack

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 import ProminentAppBar from './particles/Header';
 import SpacingGrid from './prjctsPage';
+import HomePage from './homePage';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <ProminentAppBar/>
       <Switch>
          {/*navigation needed for homepage  */}
+        <Route exact path='/'component={HomePage}/>
         <Route exact path='/portfolio'component={SpacingGrid}/>      
       </Switch>
     </Router>
