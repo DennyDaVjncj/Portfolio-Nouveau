7b8182598b18b5b8e9c5d2bf6f545a8c8ecc15bb
Denny da Vjncj <daVjncjCode@gmail.com>
2021-06-01 16:54:03 -0400

    updated file name

--- src/HomePage.js
+++ src/HomePage.js
@@ -2,4 +2,23 @@ import React from 'react';
 import Portrait from './imagery/portfolioPic.jpg';
 import {Grid,Paper,Container,CardContent,Typography,Card,CardActionArea,CardActions,CardMedia,Button} from '@material-ui/core';
 
-let 
+let homePage=()=>{
+    return(
+        <div>
+            <Container maxWidth='sm'>
+                <Grid container display='column'alignItems='center'>
+                    <Grid item xs>                        
+                        <Card>
+                            <CardActionArea>
+                                <Paper elevation={20}>
+                                    <CardMedia component='img'alt='a fine portrait of thine own visage'image={Portrait}title='Dennis Desmornes (Denny da Vjncj)'/>
+                                </Paper>
+                            </CardActionArea>
+                        </Card>                        
+                    </Grid>
+                </Grid>
+            </Container>
+        </div>
+    )
+}
+export default homePage;
