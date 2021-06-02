edf1b21854276fd7feeeca34b7c5e595a66fab02
Denny da Vjncj <daVjncjCode@gmail.com>
2021-06-02 17:08:07 -0400

    updated homepage

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Portrait from './imagery/portfolioPic.jpg';
-import {Grid,Paper,Container,CardContent,Typography,Card,CardActionArea,CardActions,CardMedia,Button} from '@material-ui/core';
+import {Grid,Paper,Container,CardContent,Typography,Card,CardActionArea,CardActions,CardMedia,Button, Link} from '@material-ui/core';
 
 let homePage=()=>{
     return(
@@ -13,7 +13,22 @@ let homePage=()=>{
                                 <Paper elevation={20}>
                                     <CardMedia component='img'alt='a fine portrait of thine own visage'image={Portrait}title='Dennis Desmornes (Denny da Vjncj)'/>
                                 </Paper>
+                                <CardContent>
+                                    <Typography gutterBottom variant='h5'component='h5'>
+                                        Dennis Desmornes (Denny da Vjncj)
+                                    </Typography>
+                                    <Typography variant='body2'color='textPrimar'component='p'>
+                                        Recent graduate of UCF's Full Stack Web Development Program powered by 2U inc; in which the primary focus was gaining proficiency within the MERN stack. Software engineering is something I took to because of the highly creative nature invloved in programmatically finding solutions.
+                                    </Typography>
+                                </CardContent>
                             </CardActionArea>
+                            <CardActions>
+                                <Button size='small'color='inherit'>
+                                    <Link>
+                                    {/* focus on the react router Link incorporation */}
+                                    </Link>
+                                </Button>
+                            </CardActions>
                         </Card>                        
                     </Grid>
                 </Grid>
