3073fbf6cd0d57335881b38fdfdfb44e7dd4bde6
Denny da Vjncj <daVjncjCode@gmail.com>
2021-06-04 16:59:49 -0400

    tentative updates to landing page

--- src/HomePage.js
+++ src/HomePage.js
@@ -14,20 +14,31 @@ let homePage=()=>{
                                     <CardMedia component='img'alt='a fine portrait of thine own visage'image={Portrait}title='Dennis Desmornes (Denny da Vjncj)'/>
                                 </Paper>
                                 <CardContent>
-                                    <Typography gutterBottom variant='h5'component='h5'>
+                                    <Typography gutterBottom variant='h4'component='h4'>
                                         Dennis Desmornes (Denny da Vjncj)
                                     </Typography>
-                                    <Typography variant='body2'color='textPrimar'component='p'>
+                                    <Typography variant='h6'color='textPrimar'component='h6'>
                                         Recent graduate of UCF's Full Stack Web Development Program powered by 2U inc; in which the primary focus was gaining proficiency within the MERN stack. Software engineering is something I took to because of the highly creative nature invloved in programmatically finding solutions.
                                     </Typography>
                                 </CardContent>
                             </CardActionArea>
                             <CardActions>
                                 <Button size='small'color='inherit'>
-                                    <Link>
-                                    {/* focus on the react router Link incorporation */}
+                                    <Link to='/portfolio'>
+                                        Le Portfolio
                                     </Link>
                                 </Button>
+                                <Button size='small'color='inherit'>
+                                    <Link href='https://github.com'>
+                                        GitHub
+                                    </Link>
+                                </Button>
+                                <Button size='small'color='inherit'>
+                                    <Link href='https://docs.google.com/document/d/1m1zEn6LABR0NyUbHnI7u6tkg-_SB9-HPY1yprxO48OM/edit#heading=h.kjltg4y8htb'target='_blank'>
+                                        Mine Resume
+                                    </Link>
+                                </Button>
+                                {/* address version control */}
                             </CardActions>
                         </Card>                        
                     </Grid>
