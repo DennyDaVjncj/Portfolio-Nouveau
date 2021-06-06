60cbcb1a96ab6f0a37e0e496f3604c047528fa9d
Denny da Vjncj <daVjncjCode@gmail.com>
2021-06-06 00:32:50 -0400

    updated imports

--- src/HomePage.js
+++ src/HomePage.js
@@ -9,74 +9,13 @@ import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import {blue,red} from '@material-ui/core/colors'
 import Paper from '@material-ui/core/Paper'
-import Selfie from './portfolioPic.jpg'
+import Portrait from './portfolioPic.jpg'
 import {Link} from 'react-router-dom'
 import AspectRatioSharpIcon from '@material-ui/icons/AspectRatioSharp';
 import Grid from '@material-ui/core/Grid';
+import Container from '@material-ui/core/Container'
 
-<<<<<<< Updated upstream
-const useStyles=makeStyles({
-    root:{
-        opacity:.4,
-        maxWidth:500           
-    },   
-    media:{
-        height:500        
-    }
-});
-const HomePage=()=>{    
-return (
-  <div>
-    <Grid container display='row'justify='center'>
-      <Grid item xs>
-        <Card>
-          <CardActionArea>
-            <CardMedia                    
-              image={Selfie}
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="h2">
-                Denny da Vjncj (Dennis Desmornes)
-              </Typography>
-              <Typography variant="body2" color="textSecondary" component="p">
-                Software engineering has become a serious passion & professional ambition due to the creative nature involved in finding solutions
-              </Typography>
-            </CardContent>
-          </CardActionArea>
-          <CardActions>
-            <Button size="small" color="primary">
-              <Link to='/portfolio'>
-                Le portfolio
-              </Link>
-            </Button>
-            <Button size="small" color="primary">
-              <a href='https://docs.google.com/document/d/1m1zEn6LABR0NyUbHnI7u6tkg-_SB9-HPY1yprxO48OM/edit?usp=sharing'target='_blank'>
-                my Resume
-              </a>{/*pdf */}
-            <Button size="small" color="primary">
-              <a href='https://github.com/DennyDaVjncj'target='_blank'>
-                Github
-              </a>
-            </Button>
-            </Button>
-            <Button size="small" color="primary">
-              <a href='https://www.linkedin.com/in/ambiguousambition/'target='_blank'>
-                LinkedIn
-              </a>
-            </Button>          
-            <Button size="small" color="primary">
-              <a href='mailto: daVjncjCode@gmail.com'>
-                email me!
-              </a>
-            </Button>          
-          </CardActions>
-        </Card>
-      </Grid>
-    </Grid>   
-  </div>
-  );
-=======
-let homePage=()=>{
+let HomePage=()=>{
     return(
         <div>
             <Container maxWidth='sm'>
@@ -103,14 +42,14 @@ let homePage=()=>{
                                     </Link>
                                 </Button>
                                 <Button size='small'color='inherit'>
-                                    <Link href='https://github.com/DennyDaVjncj'target='_blank'>
+                                    <a href='https://github.com/DennyDaVjncj'target='_blank'>
                                         GitHub
-                                    </Link>
+                                    </a>
                                 </Button>
                                 <Button size='small'color='inherit'>
-                                    <Link href='https://docs.google.com/document/d/1m1zEn6LABR0NyUbHnI7u6tkg-_SB9-HPY1yprxO48OM/edit#heading=h.kjltg4y8htb'target='_blank'>
+                                    <a href='https://docs.google.com/document/d/1m1zEn6LABR0NyUbHnI7u6tkg-_SB9-HPY1yprxO48OM/edit#heading=h.kjltg4y8htb'target='_blank'>
                                         Mine Resume
-                                    </Link>
+                                    </a>
                                 </Button>
                                 {/* address version control */}
                             </CardActions>
@@ -120,6 +59,5 @@ let homePage=()=>{
             </Container>
         </div>
     )
->>>>>>> Stashed changes
 }
 export default HomePage;
