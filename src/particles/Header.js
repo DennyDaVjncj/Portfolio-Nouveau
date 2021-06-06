08003aca3be035629f2f086c7a040d0617585a0a
Denny da Vjncj <daVjncjCode@gmail.com>
2021-06-06 00:41:10 -0400

    removed icon buttons

--- src/particles/Header.js
+++ src/particles/Header.js
@@ -40,14 +40,6 @@ export default function ProminentAppBar() {
     <div className={classes.root}>
       <AppBar position="static"className='Header'>
         <Toolbar className={classes.toolbar}>
-          <IconButton
-            edge="start"
-            className={classes.menuButton}
-            color="inherit"
-            aria-label="open drawer"
-          >
-            <MenuIcon />
-          </IconButton>
           <Button className={classes.title}variant='text'>
             <Link to='/'>
               <Typography variant="h5" noWrap>
@@ -55,12 +47,6 @@ export default function ProminentAppBar() {
               </Typography>
             </Link>
           </Button>
-          <IconButton aria-label="search" color="inherit">
-            <SearchIcon />
-          </IconButton>
-          <IconButton aria-label="display more actions" edge="end" color="inherit">
-            <MoreIcon />
-          </IconButton>
         </Toolbar>
       </AppBar>
     </div>
