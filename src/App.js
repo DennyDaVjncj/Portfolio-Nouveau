bd4ed79b64a4cdf3b9848458b93ff63e986cae92
Denny da Vjncj <daVjncjCode@gmail.com>
2021-06-06 00:34:11 -0400

    updated imports

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import './App.css';
-import MediaCard from './HomePage';
 import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 import ProminentAppBar from './particles/Header';
-import Portfolio from './prjctsPage';
+import Portfolio from './prjctsPage.js';
+import HomePage from './HomePage.js';
 
 function App() {
   return (
     <Router basename="/">
       <ProminentAppBar/>
       <Switch>
-        <Route exact path='/'component={MediaCard}/> 
-        <Route exact path='/prjctsPage'component={Portfolio}/>      
+        <Route exact path='/'component={HomePage}/> 
+        <Route exact path='/portfolio'component={Portfolio}/>      
       </Switch>
     </Router>
   );
