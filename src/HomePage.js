33d2d22ffdd1a37ce9989fde92297c8cc91917e9
Denny da Vjncj <daVjncjCode@gmail.com>
2021-08-18 09:25:01 -0400

    fixed grammatical error

--- src/HomePage.js
+++ src/HomePage.js
@@ -30,7 +30,7 @@ let HomePage=()=>{
                                         Dennis Desmornes (Denny da Vjncj)
                                     </Typography>
                                     <Typography variant='h6'color='textPrimar'component='h6'>
-                                        Recent graduate of UCF's Full Stack Web Development Program powered by 2U inc; in which the primary focus was gaining proficiency within the MERN stack. Software engineering is something I took to because of the highly creative nature invloved in programmatically finding solutions.
+                                        Recent graduate of UCF's Full Stack Web Development Program powered by 2U inc; in which the primary focus was gaining proficiency within the MERN stack. Software engineering is something I took to because of the highly creative nature invoved in programmatically finding solutions.
                                     </Typography>
                                 </CardContent>
                             </CardActionArea>
